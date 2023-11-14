9c837e6c1106228952688e414d762fe04fad8671
HowardAT06 <137073903+HowardAT06@users.noreply.github.com>
2023-11-14 20:54:37 +0300

    Add files via upload

---
+++ laba7/laba7/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // в конце каждой строки необходимо ставить пробел(Inlet.in)
+        string content = File.ReadAllText("Inlet.in");
+        string[] words = content.Split(' ');
+
+        int maxLength = 0;
+        int maxIndex = -1;
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Length > maxLength)
+            {
+                maxLength = words[i].Length;
+                maxIndex = i;
+            }
+        }
+
+        int result = maxIndex != -1 ? maxIndex + 1 : -1;
+        File.WriteAllText("Outlet.out", result.ToString());
+        Console.WriteLine("Результат записан в файл Outlet.out.");
+    }
+}
